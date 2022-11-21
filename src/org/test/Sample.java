b908650f11c14ea5b1cbbaaa5fead6d5a315520f
Manjulameena <mailtomanjulamurugan@gmail.com>
2022-11-21 13:19:52 +0530

    send a push

---
+++ src/org/test/Sample.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class Sample {
+private void sam() {
+	System.out.println("manju");
+}
+public static void main(String[] args) {
+	Sample a = new Sample();
+	a.sam();
+	}
+}
